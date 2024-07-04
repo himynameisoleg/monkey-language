f726ec77ead4c2a94b291391a6064ff34b1634be
Oleg <heyitsoleg@gmail.com>
2024-07-04 15:52:16 -0500

    boolean literals to evaluator

--- object/object.go
+++ object/object.go
@@ -24,12 +24,12 @@ type Integer struct {
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
-type Bollean struct {
+type Boolean struct {
 	Value bool
 }
 
-func (b *Bollean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Bollean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct {
 }
