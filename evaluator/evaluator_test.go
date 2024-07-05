834ac9bcde190075c3e25b01dd1fd5487281e393
Oleg <heyitsoleg@gmail.com>
2024-07-05 08:30:12 -0500

    evaluator prefix expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestEvalIntegerExpression(t *testing.T) {
+func TesteValIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected int64
@@ -72,4 +72,23 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 		return false
 	}
 	return true
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
