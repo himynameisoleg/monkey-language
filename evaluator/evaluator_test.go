7b21c3e1250b345a328fa76e819a22718775d50e
Oleg <heyitsoleg@gmail.com>
2024-07-11 08:56:48 -0500

    add test for closures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -350,3 +350,12 @@ func testNullObject(t *testing.T, obj object.Object) bool {
 	return true
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+   let newAdder = fn(x) {
+     fn(y) { x + y };
+};
+   let addTwo = newAdder(2);
+   addTwo(2);`
+	testIntegerObject(t, testEval(input), 4)
+}
