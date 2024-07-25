007895c23df50f5d33a66d1b0e96a0d8e8f21cb8
Oleg <himynameisoleg@users.noreply.github.com>
2024-07-25 21:47:25 +0000

    "puts" command & кінець!

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,8 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/object"
-	// "unicode/utf8"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -97,6 +97,15 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
